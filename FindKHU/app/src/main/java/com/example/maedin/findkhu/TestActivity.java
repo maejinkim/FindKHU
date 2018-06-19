0317b1068adaef323153373f8ca783521a072bd6
Yoonmee <ymhwang119@naver.com>
2018-06-19 13:37:09 +0900

    testing

--- FindKHU/app/src/main/java/com/example/maedin/findkhu/TestActivity.java
+++ FindKHU/app/src/main/java/com/example/maedin/findkhu/TestActivity.java
@@ -1,125 +1,117 @@
 package com.example.maedin.findkhu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import sungkyul.ac.kr.customframework.R;
+import sungkyul.ac.kr.customframework.retrofit.RetrofitRepo;
+import sungkyul.ac.kr.customframework.retrofit.RetrofitService;
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends Fragment {
 
-    TextView tvData;
+    View mView;
+    TextView textViewIndex, textViewPost, textViewMap;
+    static final String URL = "http://www.dxmnd.com/blog/";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_retrofit, container, false);
 
-        tvData = (TextView)findViewById(R.id.textView);
-        Button btn = (Button)findViewById(R.id.httpTest);
+        textViewIndex = (TextView)mView.findViewById(R.id.txtRetrofitTest);
+        textViewPost = (TextView)mView.findViewById(R.id.txtRetrofitPost);
+        textViewMap = (TextView)mView.findViewById(R.id.txtRetrofitMap);
 
-        //버튼이 클릭되면 여기 리스너로 옴
-        btn.setOnClickListener(new View.OnClickListener() {
+        index();
+        post();
+        map();
 
+        return mView;
+    }
+
+
+    public void index() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RetrofitService retrofitService = retrofit.create(RetrofitService.class);
+        Call<RetrofitRepo> call = retrofitService.getIndex("mos");
+        call.enqueue(new Callback<RetrofitRepo>() {
+            @Override
+            public void onResponse(Call<RetrofitRepo> call, Response<RetrofitRepo> response) {
+                RetrofitRepo repo = response.body();
+                textViewIndex.setText(repo.getName());
+            }
 
             @Override
-            public void onClick(View view) {
-                new JSONTask().execute("http://192.168.0.6:8000/post");//AsyncTask 시작시킴
+            public void onFailure(Call<RetrofitRepo> call, Throwable t) {
+
             }
         });
     }
 
-    public class JSONTask extends AsyncTask<String, String, String>{
+    public void post() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        @Override
-        protected String doInBackground(String... urls) {
-            try {
-                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
-                JSONObject jsonObject = new JSONObject();
-                jsonObject.accumulate("user_id", "androidTest");
-                jsonObject.accumulate("name", "yun");
-
-                HttpURLConnection con = null;
-                BufferedReader reader = null;
-
-                try{
-                    //URL url = new URL("http://192.168.0.6:8000/users");
-                    URL url = new URL(urls[0]);
-                    //연결을 함
-                    con = (HttpURLConnection) url.openConnection();
-
-                    con.setRequestMethod("POST");//POST방식으로 보냄
-                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
-                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
-                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
-                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
-                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
-                    con.connect();
-
-                    //서버로 보내기위해서 스트림 만듬
-                    OutputStream outStream = con.getOutputStream();
-                    //버퍼를 생성하고 넣음
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
-                    writer.write(jsonObject.toString());
-                    writer.flush();
-                    writer.close();//버퍼를 받아줌
-
-                    //서버로 부터 데이터를 받음
-                    InputStream stream = con.getInputStream();
-
-                    reader = new BufferedReader(new InputStreamReader(stream));
-
-                    StringBuffer buffer = new StringBuffer();
-
-                    String line = "";
-                    while((line = reader.readLine()) != null){
-                        buffer.append(line);
-                    }
-
-                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
-
-                } catch (MalformedURLException e){
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if(con != null){
-                        con.disconnect();
-                    }
-                    try {
-                        if(reader != null){
-                            reader.close();//버퍼를 닫아줌
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+        RetrofitService retrofitService = retrofit.create(RetrofitService.class);
+        Call<RetrofitRepo> call = retrofitService.getPost("post");
+        call.enqueue(new Callback<RetrofitRepo>() {
+            @Override
+            public void onResponse(Call<RetrofitRepo> call, Response<RetrofitRepo> response) {
+                RetrofitRepo repo = response.body();
+                textViewPost.setText(repo.getOutput());
             }
 
-            return null;
-        }
+            @Override
+            public void onFailure(Call<RetrofitRepo> call, Throwable t) {
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            tvData.setText(result);//서버로 부터 받은 값을 출력해주는 부
-        }
+            }
+        });
+    }
+
+    public void map() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Map map = new HashMap();
+        map.put("name","map");
+
+        RetrofitService retrofitService = retrofit.create(RetrofitService.class);
+        Call<RetrofitRepo> call = retrofitService.getItem(map);
+        call.enqueue(new Callback<RetrofitRepo>() {
+            @Override
+            public void onResponse(Call<RetrofitRepo> call, Response<RetrofitRepo> response) {
+                RetrofitRepo repo = response.body();
+                String a = "";
+                for(int i=0;i<repo.getInfo().size();i++) {
+                    a += repo.getInfo().get(i).getName() + "\n";
+                    a += repo.getInfo().get(i).getOutput();
+                }
+                textViewMap.setText(a);
+            }
+
+            @Override
+            public void onFailure(Call<RetrofitRepo> call, Throwable t) {
+
+            }
+        });
     }
 
     }
