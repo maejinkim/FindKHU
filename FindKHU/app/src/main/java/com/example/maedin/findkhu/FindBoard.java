e829056d398ef241f0c6a25ec4c2473f035b240f
maejinkim <sikhye823@gmail.com>
2018-06-19 11:31:31 +0900

    FindBoard.java 내에 버튼 적용 방법 구현

--- FindKHU/app/src/main/java/com/example/maedin/findkhu/FindBoard.java
+++ FindKHU/app/src/main/java/com/example/maedin/findkhu/FindBoard.java
@@ -1,17 +1,38 @@
 package com.example.maedin.findkhu;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class FindBoard extends Fragment {
+public class FindBoard extends Fragment implements View.OnClickListener {
+
+    View view;
+    Button btn;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.find_board, container, false);
+
+        view = inflater.inflate(R.layout.find_board, container, false);
+
+        btn = (Button) view.findViewById(R.id.btn_find_post);
+        btn.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_find_post)
+        {
+            Activity root = getActivity();
+            Toast.makeText(root, "클릭", Toast.LENGTH_LONG).show();
+        }
     }
 }
