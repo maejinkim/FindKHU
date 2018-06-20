fa9381d13cc0cbc3095b41878cb2241f6bcc9315
Yoonmee <ymhwang119@naver.com>
2018-06-20 22:10:11 +0900

    꾸엉

--- FindKHU/app/src/main/java/com/example/maedin/findkhu/fragment/MyPage.java
+++ FindKHU/app/src/main/java/com/example/maedin/findkhu/fragment/MyPage.java
@@ -2,7 +2,7 @@ package com.example.maedin.findkhu.fragment;
 
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
