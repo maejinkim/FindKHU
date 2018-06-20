9c520645eb3c0f5040407f3d6e520135ccd49e85
maejinkim <sikhye823@gmail.com>
2018-06-21 03:20:29 +0900

    new clone

---
+++ FindKHU/app/src/main/java/com/example/maedin/findkhu/fragment/ViewDetail.java
@@ -0,0 +1,22 @@
+package com.example.maedin.findkhu.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.maedin.findkhu.R;
+
+public class ViewDetail extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.view_detail, container, false);
+        return view;
+    }
+}
