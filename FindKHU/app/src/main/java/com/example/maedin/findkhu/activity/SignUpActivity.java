0b4e41b68da9a1a478e6546b074bd4b3b18ad201
maejinkim <sikhye823@gmail.com>
2018-06-20 22:08:52 +0900

    ddd

--- FindKHU/app/src/main/java/com/example/maedin/findkhu/activity/SignUpActivity.java
+++ FindKHU/app/src/main/java/com/example/maedin/findkhu/activity/SignUpActivity.java
@@ -65,29 +65,30 @@ public class SignUpActivity extends Activity implements View.OnClickListener{
                 Log.e("[index화면] 3.1 계정 불러오기", "성공");
                 MemberInfoItem item = response.body();
                 if(response.isSuccessful() && !StringLib.getInstance().isBlank(item.name)){
+                    Toast.makeText(SignUpActivity.this, "존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show();
                     Log.e("[index화면] 3.2 계정 불러오기", "성공, 기존 계정 확인");
-                    setMemberInfo(item);
+//                    setMemberInfo(item);
                 } else {
-                    Toast.makeText(IndexActivity.this, "계정이 없습니다. 프로필 창으로 넘어갑니다", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this, "계정이 없습니다. 프로필 창으로 넘어갑니다", Toast.LENGTH_SHORT).show();
                     Log.e("[index화면] 3.2 계정 불러오기", "성공, 하지만 기존 계정 없음");
-                    goProfileActivity(item);
+//                    goProfileActivity(item);
                 }
             }
             @Override
             public void onFailure(Call<MemberInfoItem> call, Throwable t) {
                 Log.e("[index화면] 3. 계정 불러오기", "서버 통신에 실패");
-                Toast.makeText(IndexActivity.this, "서버 통신에 실패했습니다", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignUpActivity.this, "서버 통신에 실패했습니다.", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    /**
-     * 사용자 계정 확인3. 서버로부터 데이터 받아오는데 성공했다면 현재 앱에 계정 정보 저장
-     */
-    private void setMemberInfo(MemberInfoItem item){
-        ((MyApp) getApplicationContext()).setMemberInfoItem(item);
-        startMain();
-    }
+//    /**
+//     * 사용자 계정 확인3. 서버로부터 데이터 받아오는데 성공했다면 현재 앱에 계정 정보 저장
+//     */
+//    private void setMemberInfo(MemberInfoItem item){
+//        ((MyApp) getApplicationContext()).setMemberInfoItem(item);
+//        startMain();
+//    }
 
     /**
      * ******경   축******
@@ -100,27 +101,27 @@ public class SignUpActivity extends Activity implements View.OnClickListener{
         finish();
     }
 
-    /**
-     * 계정 불러오기에 실패한경우
-     */
-    private void goProfileActivity(MemberInfoItem item){
-
-        // 만약 처음 등록한 사람 즉, 계정이 없는 사람이라면 번호를 등록해준다.
-        if(item == null || item.seq <= 0){
-            Log.e("[index화면] 4.1 계정 없음", "계정 저장 시도");
-            insertMemberInfo();
-        }
-
-        // 메인 액티비티 띄운 다음
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-
-        // 프로필 액티비티에서 계정 작성 하도록 한다
-        Intent intent2 = new Intent(this, ProfileActivity.class);
-        startActivity(intent2);
-
-        finish();
-    }
+//    /**
+//     * 계정 불러오기에 실패한경우
+//     */
+//    private void goProfileActivity(MemberInfoItem item){
+//
+//        // 만약 처음 등록한 사람 즉, 계정이 없는 사람이라면 번호를 등록해준다.
+//        if(item == null || item.id <= 0){
+//            Log.e("[index화면] 4.1 계정 없음", "계정 저장 시도");
+//            insertMemberInfo();
+//        }
+//
+//        // 메인 액티비티 띄운 다음
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//
+//        // 프로필 액티비티에서 계정 작성 하도록 한다
+//        Intent intent2 = new Intent(this, ProfileActivity.class);
+//        startActivity(intent2);
+//
+//        finish();
+//    }
 
     /**
      * 계정이 없는 경우 새로 서버에 등록한다.
@@ -135,11 +136,11 @@ public class SignUpActivity extends Activity implements View.OnClickListener{
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 if(response.isSuccessful()){
-                    Toast.makeText(IndexActivity.this, "번호 등록 성공", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this, "번호 등록 성공", Toast.LENGTH_SHORT).show();
                     Log.e("[index화면] 4.2 계정 없음", "계정 저장 성공");
                 } else {
                     int statusCode = response.code();
-                    Toast.makeText(IndexActivity.this, statusCode+" 오류 발생", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this, statusCode+" 오류 발생", Toast.LENGTH_SHORT).show();
                     ResponseBody errbody = response.errorBody();
                     Log.e("[index화면] 4.2 계정 없음", "계정 저장 실패 "+statusCode);
                     Log.e("[index화면] 4.2 계정 없음", errbody.toString());
@@ -147,7 +148,7 @@ public class SignUpActivity extends Activity implements View.OnClickListener{
             }
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(IndexActivity.this, "서버 통신에 실패했습니다", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignUpActivity.this, "서버 통신에 실패했습니다", Toast.LENGTH_SHORT).show();
                 Log.e("[index화면] 4.2 계정 없음", "계정 저장 서버 통신 실패");
             }
         });
